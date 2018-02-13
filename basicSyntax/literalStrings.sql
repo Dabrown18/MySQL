890f3ed0f6840f3381c0d5e30845aca2e2764a7b
Darron Brown <DarronBrown88@outlook.com>
2018-02-13 12:48:17 -0700

    literal strings

---
+++ basicSyntax/literalStrings.sql
@@ -0,0 +1,9 @@
+-- 11 literal strings
+-- database: [none]
+
+SELECT 'hello, world';
+SELECT 'hello' ', ' 'world';
+SELECT "hello, world";
+SELECT 'hello, "world"';
+SELECT 'hello, ''world''';
+SELECT 'hello, \'world\'';
